8989e79280628c67fb3c199f527754768848df22
Srujan Bidgar <139164617+srujan-bidgar@users.noreply.github.com>
2023-10-05 20:04:13 +0530

    Add files via upload

---
+++ bruteforce.cpp
@@ -0,0 +1,97 @@
+// DS used: vectors
+// implementing bruteforce LRU
+// ----------------------------
+// 
+//      CONSIDERING
+//
+//-----------------------------
+// end of vector = most recently used pair
+// front of vector = least recently used pair
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Cache
+{
+	public:
+		int capacity;
+			
+		virtual void evict_key_and_return(){}; 
+		virtual int get(int key){};
+		virtual void put(int key, int value){};
+
+};
+
+class BruteLRU: public Cache
+{
+	public:
+  
+	vector<pair<int, int>>v; //vector of pairs
+ 	BruteLRU(int _capacity)
+	{
+		capacity = _capacity; 
+		v.clear();
+	}
+	bool check_rearrange(int key)          //true = if the element is present & rearrange : false = not present
+	{
+		pair<int, int> ans;
+
+		for(int i = 0; i < v.size(); i++)  //max size of v = capacity
+		{
+			if(v[i].first == key)
+			{
+				ans = v[i];
+				v.erase(v.begin() + i);   //deleting the ith entry (key,value) 
+				v.push_back(ans);         //positioning ans at the end 
+				return 1;
+			}
+		}
+		return 0; //invalid index
+	}
+	void evict_key_and_return()
+	{
+		int x = v[0].first;
+		v.erase(v.begin());
+		
+	}
+ 
+	int get(int key)
+	{
+	    if( check_rearrange(key) == 1)
+	    	return v.back().second;
+		return -1;
+
+	}
+
+	void put(int key, int value)
+	{
+		pair<int, int> ans;
+		if(check_rearrange(key) == 1)
+		{
+			v.back().second = value;
+			return;
+		}
+		
+		if(v.size() == capacity)
+			evict_key_and_return();
+
+		v.push_back({key, value});
+		return;
+	}
+	
+	
+};
+
+int main() 
+{
+	
+	BruteLRU c = BruteLRU(2);
+	cout << c.get(1) << endl;
+	c.put(3, 5);
+	c.put(1, 6);
+	c.put(4, 8);
+	cout << c.get(3) << endl;
+	cout << c.get(4) << endl;	
+	return 0;
+}
---
+++ optimized.cpp
@@ -0,0 +1,122 @@
+// optimised solution
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Cache
+{
+public:
+    int cap;
+    // int size;
+
+    virtual int evict_key_and_return(){}; // virtual [keyword] : compulsory for derived classes to implement
+    virtual int get(int key){};
+    virtual void put(int key, int value){};
+};
+
+class LRUcache : public Cache
+{
+public:
+    class node
+    {
+    public:
+        int key;
+        int val;
+        node *next;
+        node *prev;
+
+        node(int _key, int _value)
+        {
+            key = _key;
+            val = _value;
+        }
+    };
+    node *head = new node(-1, -1);
+    node *tail = new node(-1, -1);
+
+    unordered_map<int, node *> mp;
+    LRUcache(int _capacity)
+    {
+        cap = _capacity;
+        mp.clear();
+        head->next = tail;
+        tail->prev = head;
+    }
+    void addnode(node *newnode)
+    {
+        node *temp = head->next;
+        newnode->next = temp;
+        newnode->prev = head;
+        head->next = newnode;
+        temp->prev = newnode;
+    }
+    void deletenode(node *delnode)
+    {
+        node *delprev = delnode->prev;
+        node *delnext = delnode->next;
+
+        delprev->next = delnext;
+        delnext->prev = delprev;
+    }
+
+    int evict_key_and_return()
+    {
+        int x = tail->prev->key;
+        mp.erase(tail->prev->key);
+        deletenode(tail->prev);
+
+        return x;
+    }
+
+    int get(int key_)
+    {
+        if (mp.find(key_) != mp.end())
+        {
+            node *foundnode = mp[key_];
+            int found = foundnode->val;
+            mp.erase(key_);
+            deletenode(foundnode);
+            addnode(foundnode);
+
+            mp[key_] = head->next;
+            return found;
+        }
+
+        return -1;
+    }
+
+    void put(int key_, int value_)
+    {
+        if (mp.find(key_) != mp.end())
+        {
+            node *existingnode = mp[key_];
+            mp.erase(key_);
+            deletenode(existingnode);
+        }
+        if (mp.size() == cap)
+        {
+            evict_key_and_return();
+        }
+
+        addnode(new node(key_, value_));
+        mp[key_] = head->next;
+    }
+};
+
+int main()
+{
+
+    LRUcache lr = LRUcache(3);
+    cout << lr.get(1) << endl;
+    lr.put(-3, 5);
+    lr.put(1, 4);
+    lr.put(1, 7);
+    cout << lr.get(1) << endl;
+    lr.put(45, 1);
+    lr.put(7, 1);
+    cout << lr.get(-3) << endl;
+    cout << lr.get(7) << endl;
+    assert(1 == lr.get(45));
+
+    return 0;
+}
